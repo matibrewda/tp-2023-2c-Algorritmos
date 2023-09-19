1f0e7f01a677636559aeea92c0afb9e47f689632
JulianRuival <jruival@frba.utn.edu.ar>
2023-09-19 22:41:19 +0000

    avances

--- Shared/utilidades_archivos.c
+++ Shared/utilidades_archivos.c
@@ -1,4 +1,6 @@
 #include "Headers/utilidades_archivos.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 char* leer_archivo(t_log* logger, char* ruta_archivo)
 {
@@ -19,4 +21,42 @@ char* leer_archivo(t_log* logger, char* ruta_archivo)
     fclose(fp);
     
     return text;
+}
+
+FILE* abrir_archivo(t_log* logger, char* ruta_archivo){
+    FILE* fp = fopen(ruta_archivo, "r+");
+
+    if (fp == NULL)
+	{
+        log_error(logger, "Error al leer archivo con la siguiente ruta: %s", ruta_archivo);
+        return NULL;
+	}
+    return fp;
+}
+
+void cerrar_arcivo(t_log* logger,FILE* archivo){
+    if(archivo != NULL){
+        fclose(archivo);
+        return;
+    }
+    log_error(logger,"Error al cerrar el archivo");
+}
+
+char* leer_linea(t_log* logger,FILE* archivo){
+    if (archivo == NULL)
+	{
+        log_error(logger,"No existe este archivo para leer");
+        return NULL;
+	}
+
+    char* linea = NULL;
+    size_t lenght = 0;
+    ssize_t read;
+
+    read = getline(&linea, &lenght, archivo);
+    if (read == -1){
+        free(linea);
+        return NULL;
+    }
+    return linea;
 }
