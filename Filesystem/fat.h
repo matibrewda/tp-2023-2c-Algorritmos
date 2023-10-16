782f7342fb43baeb8cfc992353c538588b99719c
Lucho Luduena <lluduena@frba.utn.edu.ar>
2023-10-16 22:20:19 +0000

    Agrego FCB y FAT structures. Firmas de funciones.

---
+++ Filesystem/fat.h
@@ -0,0 +1,20 @@
+#ifndef FAT_H_
+#define FAT_H_
+
+#include "commons/config.h"
+#include "commons/log.h"
+#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+//ubicar Struct y HEADERS de funciones de utilidad
+
+typedef struct {
+    uint32_t block_value;  // Valor de la entrada de la FAT
+} FATEntry;
+
+int iniciarFAT (t_log* logger,char* fat_path, uint32_t cant_bloques_total, uint32_t cant_bloques_swap);
+
+#endif /* FAT_H_ */
