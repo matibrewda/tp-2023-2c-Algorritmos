bf254e88a56b1d3dbe94610f71936dfdf199c241
Matias Nuñez <matiasne45@gmail.com>
2023-12-04 21:43:37 +0000

    agrego scripts para deploy local

--- deploy_base.sh
+++ deploy_base.sh
@@ -4,18 +4,21 @@ cd Memoria
 make limpiar
 make compilar
 nohup ./Ejecutable/memoria.o Configuracion/memoria_prueba_base.config &
+cd ..
 
 cd Filesystem
 make limpiar
 make compilar
 nohup ./Ejecutable/filesystem.o Configuracion/filesystem_prueba_base.config &
+cd ..
 
 cd CPU
 make limpiar
 make compilar
 nohup ./Ejecutable/cpu.o Configuracion/cpu_prueba_base.config &
+cd ..
 
 cd Kernel
 make limpiar
 make compilar
-nohup ./Ejecutable/kernel.o Configuracion/kernel_prueba_base.config &
+./Ejecutable/kernel.o Configuracion/kernel_prueba_base.config &
---
+++ stop_all_process.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd Memoria
+make detener
+cd ..
+
+cd Filesystem
+make detener
+cd ..
+
+cd CPU
+make detener
+cd ..
+
+cd Kernel
+make detener
